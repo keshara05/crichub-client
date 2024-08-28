cd77f9e50ee484087b65f092d96944a1feb69670
Nilushka-Poornima <nilushkapoornima169@gmail.com>
2024-08-28 11:37:41 +0530

    password validate character count, responsive for window size, leave a note section built

--- src/Components/Pages/Login/Login.jsx
+++ src/Components/Pages/Login/Login.jsx
@@ -3,38 +3,44 @@ import './Login.css';
 import { FaUserAlt, FaLock } from "react-icons/fa";
 import cricketerLogo from '../../Assets/Cricketer logo.png';
 
+const openGmail = () => {
+  const recipient = 'CrickHub@gmail.com';
+  const mailtoUrl = `https://mail.google.com/mail/?view=cm&fs=1&to=${recipient}`;
+  window.open(mailtoUrl, '_blank');
+};
 
 const Login = () => {
   return (
     <div className='main-login-page'>
       <div className='main-image'>
-      <img src={cricketerLogo} alt="Cricketer Logo" />
+        <img src={cricketerLogo} alt="Cricketer Logo" />
       </div>
       <div className='wrapper'>
-      <form action="">
-        <h1>Sign in</h1>
-        <div className='input-box'>
-          <input type="email" placeholder='E-mail' required />
-          <FaUserAlt className='icon' />
-        </div>
-        <div className='input-box'>
-          <input type="password" placeholder='Password' minlength="8" required />
-          <FaLock className='icon' />
-        </div>
+        <form action="">
+          <h1>Sign in</h1>
+          <div className='input-box'>
+            <input type="email" placeholder='E-mail' required />
+            <FaUserAlt className='icon' />
+          </div>
+          <div className='input-box'>
+            <input type="password" placeholder='Password' minlength="8" required />
+            <FaLock className='icon' />
+          </div>
 
-        <div className='remember-forgot'>
-          <label><input type="checkbox" />Remember me</label>
-          <a href="#">Forgot password?</a>
-        </div>
+          <div className='remember-forgot'>
+            <label><input type="checkbox" />Remember me</label>
+            <a href="#">Forgot password?</a>
+          </div>
 
-        <button type='submit'>Sign in</button>
+          <button type='submit'>Sign in</button>
 
-        <div className='register-link'>
-          <p>Holy! you are unable to create a new account Please contact Administrator. <a href="mailto:nilushkapoornima@gmail.com?subject=Issue%20with%20Account%20Creation&body=I%20encountered%20an%20issue%20while%20trying%20to%20create%20a%20new%20account.%20Please%20assist.">
-      Leave a note
-   </a></p>
-        </div>
-      </form>
+          <div className='leave-note'>
+            <p>Holy! You are unable to create a new account. Please contact the Administrator.
+              <a onClick={openGmail}> Leave a note </a>
+            </p>
+
+          </div>
+        </form>
       </div>
 
     </div>
